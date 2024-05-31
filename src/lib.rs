51eff64e5e26d0e9b86a851aaea4dd2b7d850bc9
Guillem Arias Fauste <hello@gariasf.com>
2024-05-31 23:02:53 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ impl<'a> State<'a> {
         let size = window.inner_size();
 
         // The instance is a handle to our GPU
-        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        // Backends::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             #[cfg(not(target_arch = "wasm32"))]
             backends: wgpu::Backends::PRIMARY,
@@ -68,23 +68,22 @@ impl<'a> State<'a> {
             .unwrap();
         let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
-        let surface_caps = surface.get_capabilities(&adapter);
-        // Shader code in this tutorial assumes an Srgb surface texture. Using a different
-        // one will result all the colors comming out darker. If you want to support non
-        // Srgb surfaces, you'll need to account for that when drawing to the frame.
-        let surface_format = surface_caps
+        let surface_capabilities = surface.get_capabilities(&adapter);
+  
+        let surface_format = surface_capabilities
             .formats
             .iter()
             .copied()
             .find(|f| f.is_srgb())
-            .unwrap_or(surface_caps.formats[0]);
+            .unwrap_or(surface_capabilities.formats[0]);
+        
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface_format,
             width: size.width,
             height: size.height,
-            present_mode: surface_caps.present_modes[0],
-            alpha_mode: surface_caps.alpha_modes[0],
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
             desired_maximum_frame_latency: 2,
             view_formats: vec![],
         };
